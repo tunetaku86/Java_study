87014e61ab20384d88b2ff94a38f57b75054b779
tune <tune@192.168.3.11>
2018-09-29 23:14:18 +0900

    orderby,inserttest2ついか

--- src/jdbc_postgresql_test/OerderByTest.java
+++ src/jdbc_postgresql_test/OerderByTest.java
@@ -3,11 +3,13 @@ package jdbc_postgresql_test;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class OerderByTest {
 
 	public static void main(String[] args) {
+
 	// 年齢の低い順に表示するプログラム
 
 		//変数の定義
@@ -23,12 +25,18 @@ public class OerderByTest {
 		//データベースへの接続
 			con = DriverManager.getConnection(url, user, pass);
 		//SQL文の作成
-			String sql = "SELECT * FROM emp ORDER BY age DESC;";
+			String sql = "SELECT * FROM emp ORDER BY age;";
 		//PreparedStatementオブジェクトの取得
 			st = con.prepareStatement(sql);
 		//SQLの実行
-			st.executeUpdate();
-
+			ResultSet rs = st.executeQuery();
+		//結果の取得および表示
+			while (rs.next()) {
+				System.out.print(rs.getInt("code")+ ":");
+				System.out.print(rs.getString("name")+ ":");
+				System.out.print(rs.getInt("age")+ ":");
+				System.out.println(rs.getString("tel"));
+			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
@@ -47,11 +55,8 @@ public class OerderByTest {
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
-
-			}
 			}
 		}
-
 	}
-
+}
 }
